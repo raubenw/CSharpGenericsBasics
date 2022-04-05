5de731695c601dc43555be54516a41ebe4edb031
raubenw <91202958+raubenw@users.noreply.github.com>
2022-04-05 20:09:05 +0200

    Eerste Commit

---
+++ Person.cs
@@ -0,0 +1,13 @@
+namespace CSharpGenericsBasics {
+
+    public class Person {
+
+        public String Name {get; set;}
+        public int Age {get; set;}
+
+        public Person(string aName, int someAge){
+            Name = aName;
+            Age = someAge;
+        }    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSharpGenericsBasics {
+
+    public class Program {
+
+        static void Main(string[] args) {
+
+            int x = 5;
+            int y = 7;
+
+            swap<int>(ref x, ref y);
+
+            Console.WriteLine($"\nValue of x is {x} and the value of y is {y}");
+
+            Person p1 = new Person("Werner", 42);
+            Person p2 = new Person("Jeanette", 37);
+
+            swap<Person>(ref p1, ref p2);
+
+            Console.WriteLine($"The value of person 1 is {p1.Name} and the value of person 2 is {p2.Name}");
+
+            Console.ReadLine();
+
+        }
+
+        public static void swap<T> (ref T obj1, ref T obj2) {
+            T temp = obj1;
+            obj1 = obj2;
+            obj2 = temp;
+        }
+    }
+}
